cbbe2c64f25e10ecd238f4252c317ad9ec94726b
supremeCucuber <942129374@qq.com>
2019-09-25 19:49:01 +0800

    infomange

---
+++ MyMaven/src/main/java/com/huanu/HelloController.java
@@ -0,0 +1,15 @@
+package com.huanu;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by sang on 2018/7/4.
+ */
+@RestController
+public class HelloController {
+    @GetMapping("/hello")
+    public String hello() {
+        return "彭文迪 201841882311";
+    }
+}
